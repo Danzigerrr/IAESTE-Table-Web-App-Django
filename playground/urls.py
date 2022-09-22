3153f72c38c551b7b78e00b255f7892102f967d0
Krzysztof Nazar <s184698@student.pg.edu.pl>
2022-09-22 14:13:24 +0200

    links to detailed offer added

--- playground/urls.py
+++ playground/urls.py
@@ -6,7 +6,7 @@ from playground.views import PersonListView
 # URL conf
 urlpatterns = [
     path('hello/', views.say_hello),
-    path('hello/<int:question_id>', views.detail, name = 'detail'),
+    path('hello/<str:question_id>', views.detail, name = 'detail'),
     path('hello/<int:question_id>/vote', views.vote, name = 'vote'),
     path('hello/<int:question_id>/results', views.results, name='results'),
     path("people/", PersonListView.as_view(), name="people")
--- playground/views.py
+++ playground/views.py
@@ -11,29 +11,30 @@ def clearHTMLCode(htmlCode):
     return result
 
 
-def addSortingButtons(newHTML):
+def addSortingButtons(newHTML, df):
     newHTML = newHTML.replace("<table ", '<table id="myTable" ')
-    newHTML = newHTML.replace("<th>RefNo</th>", '<th onclick="sortTable(0)">RefNo</th>')
-    newHTML = newHTML.replace("<th>City</th>", '<th onclick="sortTable(0)">City</th>')
-    newHTML = newHTML.replace("<th>Deadline</th>", '<th onclick="sortTable(0)">Deadline</th>')
+    for col in df.columns:
+        newHTML = newHTML.replace("<th>"+col+"</th>", '<th onclick="sortTable(0)">' + col + '</th>')
     print(newHTML)
     return newHTML
 
 
-def deleteIDColumn(newHTML, nrows):
+def deleteIDColumn(newHTML, nrows=2):
     newHTML = newHTML.replace("<th></th>", '')
     for i in range(nrows):
         id = str(i)
-        newHTML = newHTML.replace("<th>"+id+"</th>", '')
+        newHTML = newHTML.replace("<th>" + id + "</th>", '')
     return newHTML
 
 
 def addLinksToRefNo(newHTML, RNList):
     for i in range(len(RNList)):
         id = str(RNList[i])
-        newHTML = newHTML.replace("<td>"+id+"</td>", '')
+        ipaddress = "http://127.0.0.1:8000/playground/hello/" + id
+        href = '<a href="' + ipaddress + '" target="_blank">' + id + '</a>'
+        currrentTD = "<td>" + id + "</td>"
+        newHTML = newHTML.replace(currrentTD, '<td>' + href + '</td>')
     return newHTML
-
 
 
 def say_hello(request):
@@ -43,9 +44,8 @@ def say_hello(request):
     df = df[["RefNo", "City", "Deadline"]]
     newHTML = clearHTMLCode(df.to_html())
     newHTML = deleteIDColumn(newHTML, nrows)
-    newHTML = addSortingButtons(newHTML)
-    #newHTML = addLinksToRefNo(newHTML, df["RefNo"])
-
+    newHTML = addSortingButtons(newHTML, df)
+    newHTML = addLinksToRefNo(newHTML, df["RefNo"])
 
     with open('test.txt', 'w') as f:
         f.write(newHTML)
@@ -53,15 +53,14 @@ def say_hello(request):
     return render(request, "playground_view_1.html", {"table": newHTML})
 
 
-
-
-
-
 def detail(request, question_id):
-    return HttpResponse("This is the detail view of the question: %s" % question_id)
-
-
-
+    import pandas as pd
+    OfferId = question_id
+    df = pd.read_csv("AllOffers_21_09_2022.csv")
+    rowWithOffer = df.loc[df['RefNo'] == OfferId]
+    newHTML = clearHTMLCode(rowWithOffer.to_html())
+    newHTML = deleteIDColumn(newHTML)
+    return render(request, "detail_view_1.html", {"table": newHTML})
 
 
 def results(request, question_id):
@@ -79,6 +78,7 @@ from .models import Person
 from .tables import PersonTable
 from django_filters.views import FilterView
 from django_tables2.views import SingleTableMixin
+
 
 class PersonListView(SingleTableMixin, FilterView):
     table_class = PersonTable
