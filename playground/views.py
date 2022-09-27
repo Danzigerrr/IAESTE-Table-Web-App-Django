5ce37fd4311614a13a577a69a6f1049b4617fcd2
Krzysztof Nazar <s184698@student.pg.edu.pl>
2022-09-27 19:01:18 +0200

    unneeded html formating functions from views.py were deleted

--- playground/views.py
+++ playground/views.py
@@ -1,57 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# DELETE BEGIN
-def clearHTMLCode(htmlCode):
-    result = htmlCode
-    result = result.replace("['']", "")
-    result = result.replace("']", "")
-    result = result.replace("['", "")
-    result = result.replace("', '", " ")
-    return result
-
-
-def addSortingButtons(newHTML, df):
-    newHTML = newHTML.replace("<table ", '<table id="myTable" ')
-    for col in df.columns:
-        newHTML = newHTML.replace("<th>"+col+"</th>", '<th onclick="sortTable(0)">' + col + '</th>')
-    return newHTML
-
-
-def deleteIDColumn(newHTML, nrows=2):
-    newHTML = newHTML.replace("<th></th>", '')
-    for i in range(nrows):
-        id = str(i)
-        newHTML = newHTML.replace("<th>" + id + "</th>", '')
-    return newHTML
-
-
-def addLinksToRefNo(newHTML, RNList):
-    for i in range(len(RNList)):
-        id = str(RNList[i])
-        ipaddress = "http://127.0.0.1:8000/playground/hello/" + id
-        href = '<a href="' + ipaddress + '" target="_blank">' + id + '</a>'
-        currrentTD = "<td>" + id + "</td>"
-        newHTML = newHTML.replace(currrentTD, '<td>' + href + '</td>')
-    return newHTML
-
-
-def deleteNewLineSigns(newHTML):
-    return newHTML.replace('\\n', '<br>').replace('\\t', '\t')
-
-
-def addLinksToCityName(newHTML, RNList, cityNames):
-    for i in range(len(RNList)):
-        id = str(RNList[i])
-        cityName = str(cityNames[i])
-        ipaddress = "http://127.0.0.1:8000/playground/hello/" + id + "/city"
-        href = '<a href="' + ipaddress + '" target="_blank">' + cityName + '</a>'
-        currrentTD = "<td>" + cityName + "</td>"
-        newHTML = newHTML.replace(currrentTD, '<td>' + href + '</td>')
-    return newHTML
-
-# DELETE END
-
 
 def say_hello(request):
     from scripts import loadFromCSV as load
@@ -121,10 +70,7 @@ def findCityOfOfferWithId(question_id):
 
 def city(request, question_id):
     cityName = findCityOfOfferWithId(question_id)
-    print("MAP:")
-    print(cityName)
     map = createMap(cityName)
-    print("-- \n\n")
     map.save("map.html")
     html_string = map.get_root().render()
     return render(request, "city_view_1.html", {"map": html_string})
@@ -133,19 +79,7 @@ def city(request, question_id):
 def detail(request, question_id):
     from playground.models import Offer
     offer = Offer.objects.get(RefNo=question_id)
-
-
-
-
-    import pandas as pd
-    OfferId = question_id
-    df = pd.read_csv("AllOffers_21_09_2022.csv")
-    rowWithOffer = df.loc[df['RefNo'] == OfferId]
-    rowWithOffer = rowWithOffer.transpose() # to show it vertically
-    newHTML = clearHTMLCode(rowWithOffer.to_html())
-    newHTML = deleteNewLineSigns(newHTML)
-    newHTML = deleteIDColumn(newHTML)
-    return render(request, "detail_view_1.html", {"table": newHTML, "offer": offer})
+    return render(request, "detail_view_1.html", {"offer": offer})
 
 
 def results(request, question_id):
@@ -155,17 +89,3 @@ def results(request, question_id):
 def vote(request, question_id):
     tostr = str(question_id)
     return render(request, "view2.html", {"name": tostr})
-
-
-from django_tables2 import SingleTableView
-
-from .models import Person
-from .tables import PersonTable
-from django_filters.views import FilterView
-from django_tables2.views import SingleTableMixin
-
-
-class PersonListView(SingleTableMixin, FilterView):
-    table_class = PersonTable
-    model = Person
-    template_name = "../templates/people.html"
