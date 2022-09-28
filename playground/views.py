d63a116da690facb0a85ddca0be12440fda11a1b
Krzysztof Nazar <s184698@student.pg.edu.pl>
2022-09-28 13:17:15 +0200

    deleted uneeded files

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from scripts.mapHandler import *
 
 def say_hello(request):
@@ -13,16 +12,14 @@ def say_hello(request):
 
 
 def findCityOfOfferWithId(question_id):
-    import pandas as pd
-    df = pd.read_csv("AllOffers_21_09_2022.csv")
-    offer = df.loc[df['RefNo'] == question_id]
-    return str(offer["City"].item())
+    from playground.models import Offer
+    offer = Offer.objects.get(RefNo=question_id)
+    return str(offer.City)
 
 
 def city(request, question_id):
     cityName = findCityOfOfferWithId(question_id)
     map = createMap(cityName)
-    map.save("map.html")
     html_string = map.get_root().render()
     return render(request, "city_view_1.html", {"map": html_string})
 
@@ -31,11 +28,6 @@ def detail(request, question_id):
     from playground.models import Offer
     offer = Offer.objects.get(RefNo=question_id)
     return render(request, "detail_view_1.html", {"offer": offer})
-
-
-def vote(request, question_id):
-    tostr = str(question_id)
-    return render(request, "view2.html", {"name": tostr})
 
 
 def map(request):
