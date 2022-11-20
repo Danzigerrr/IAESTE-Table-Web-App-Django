81f459b286b78f3300a5e3567cb0c64540ed2625
Krzysztof Nazar <s184698@student.pg.edu.pl>
2022-11-20 23:58:08 +0100

    responsive menu added, sorting problem with button on column number 4 fixed

--- static/JavaScript/staticElemAdder.js
+++ static/JavaScript/staticElemAdder.js
@@ -1,13 +1,20 @@
 function addVerticalMenu(){
-    document.getElementById("verticalMenuElement").innerHTML += "<!-- Menu vertical on the left side -->\n" +
-        "<ul>\n" +
-        "    <li><a href=\"https://iaeste-offers-django-web-appkn.herokuapp.com/iaesteTable/offers\">Offers list</a></li>\n" +
-        "    <li><a href=\"https://iaeste-offers-django-web-appkn.herokuapp.com/iaesteTable/map\" target=\"_blank\">Offers map</a></li>\n" +
-        "    <li><a href=\"https://iaeste-offers-django-web-appkn.herokuapp.com/iaesteTable/aboutProject\" target=\"_blank\">About project</a></li>\n" +
-        "    <li><a href=\"https://iaeste-offers-django-web-appkn.herokuapp.com/iaesteTable/aboutAuthor\" target=\"_blank\">About author</a></li>\n" +
-        "</ul>";
 
-    }
+
+    document.getElementById("verticalMenuElement").innerHTML +=
+        "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">\n" +
+        "    <div class=\"topnav\" id=\"myTopnav\">\n" +
+        "    <a href=\"https://iaeste-offers-django-web-appkn.herokuapp.com/iaesteTable/offers\">Offers list</a>\n" +
+        "    <a href=\"https://iaeste-offers-django-web-appkn.herokuapp.com/iaesteTable/map\" target=\"_blank\">Offers map</a>\n" +
+        "    <a href=\"https://iaeste-offers-django-web-appkn.herokuapp.com/iaesteTable/aboutProject\" target=\"_blank\">About project</a>\n" +
+        "    <a href=\"https://iaeste-offers-django-web-appkn.herokuapp.com/iaesteTable/aboutAuthor\" target=\"_blank\">About author</a>\n" +
+        "      <a href=\"javascript:void(0);\" class=\"icon\" onclick=\"menuMyFunction()\">\n" +
+        "        <i class=\"fa fa-bars\"></i>\n" +
+        "      </a>\n" +
+        "    </div>"
+
+}
+
 
 function addAuthorFootnote(){
     document.getElementById("authorFootnote").innerHTML += "" +
@@ -15,3 +22,14 @@ function addAuthorFootnote(){
         "</p>"
 
 }
+
+
+        /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function menuMyFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
